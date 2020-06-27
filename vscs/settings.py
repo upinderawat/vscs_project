e32521df2272560ee204177810730ee4573bf001
upinder <upinderawat@gmail.com>
2020-06-27 14:51:06 +0530

    Added rest_framework in setting

--- vscs/settings.py
+++ vscs/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'users.apps.UsersConfig',
     'restaurants.apps.RestaurantsConfig',
     'django.contrib.admin',
