5ab93d2f7fa089c39c596c1f4c600d700de9e859
Pradeep Pallikila <pradeeppallikila1234@gmail.com>
2020-06-27 13:10:49 +0530

    url file uploaded

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('registerUser', views.registerUser.as_view()),
+    path('checkUserLogin', views.checkUser.as_view())
+]
