d70743a5bc1f818f138086446bc75eea97e9064d
Pradeep Pallikila <pradeeppallikila1234@gmail.com>
2020-06-27 13:13:43 +0530

    User model updated

--- users/models.py
+++ users/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    email= models.CharField(max_length=100)
+    email= models.CharField(max_length=100, unique=True)
     name= models.CharField(max_length=100)
-    cardNumber= models.CharField(max_length=100)
+    cardNumber= models.CharField(max_length=16, unique=True)
     password= models.CharField(max_length=100)
     address= models.CharField(max_length=100)
+    
+    class Meta:
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
